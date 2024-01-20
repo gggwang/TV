f55e079b3e04b3e0c1cda00c70a76e0b31e29698
devops@intramirror.com <devops@intramirror.com>
2024-01-20 12:08:01 +0800

    update

--- test.py
+++ test.py
@@ -1,13 +1,51 @@
 a=r'''{
-      "key": "厂长",
-      "name": "🏭┃厂长影视",
+      "key": "玩偶哥哥",
+      "name": "👽|玩偶[阿里云盘]",
       "type": 3,
-      "jar": "https://agit.ai/2011820123/tv/raw/branch/master/jar/Yoursmile.jar;md5;2E59A2E94BD70ADE173785BB016A61C9",
-      "playerType": "2",
-      "api": "https://d.kstore.space/download/6296/js/lib/drpy2.min.js",
-      "ext": "https://d.kstore.space/download/6296/js/厂长.js",
+      "jar": "https://www.qiqiguai.cn/tv/3/fty/fty.jar;md5;89d697916e9b66cf6b9bfd81981f274d",
+      "api": "csp_WoGG",
+      "searchable": 1,
+      "quickSearch": 1,
+      "changeable": 0,
+      "ext": "http://127.0.0.1:9978/file/tvfan/token.txt+4k|auto|fhd$$$https://api.wogg.xyz/$$$弹"
+    },
+    {
+      "key": "77",
+      "name": "👒七七┃App",
+      "type": 3,
+      "jar": "https://www.qiqiguai.cn/tv/3/fty/fty.jar;md5;89d697916e9b66cf6b9bfd81981f274d",
+      "api": "csp_Kunyu77",
+      "searchable": 1,
+      "quickSearch": 1,
+      "changeable": 1
+    },
+    {
+      "key": "虎牙直播js",
+      "name": "🐯虎牙┃直播",
+      "type": 3,
+      "jar": "https://www.qiqiguai.cn/tv/3/fty/fty.jar;md5;89d697916e9b66cf6b9bfd81981f274d",
+      "api": "https://www.qiqiguai.cn/tv/3/fty/lib/drpy2.min.js",
+      "ext": "https://www.qiqiguai.cn/tv/3/fty/lib/虎牙直播.js",
+      "style": {
+        "type": "list"
+      },
       "searchable": 0,
-      "quickSearch": 0
+      "quickSearch": 0,
+      "changeable": 0
+    },
+    {
+      "key": "csp_XYQBiu_斗鱼",
+      "name": "🐟斗鱼┃直播",
+      "type": 3,
+      "jar": "https://www.qiqiguai.cn/tv/3/fty/fty.jar;md5;89d697916e9b66cf6b9bfd81981f274d",
+      "api": "https://www.qiqiguai.cn/tv/3/fty/lib/drpy2.min.js",
+      "ext": "https://www.qiqiguai.cn/tv/3/fty/lib/斗鱼直播.js",
+      "style": {
+        "type": "list"
+      },
+      "searchable": 0,
+      "quickSearch": 0,
+      "changeable": 0
     },'''
 
 b=a.replace(' ','').replace('\t','').replace('\n','')
